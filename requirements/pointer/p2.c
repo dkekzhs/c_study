1ccd3b0ccbcd85ad647a47f059a3c9e167fb9f9a
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2024-11-15 00:37:11 +0900

    동적 메모리 2번

--- requirements/pointer/p2.c
+++ requirements/pointer/p2.c
@@ -1,1 +1,23 @@
 // 동적 메모리로 100바이트를 할당 받아 알파벳을 출력해봅니다.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    char *cp;
+    cp = (char *)malloc(sizeof(char) * 100);
+
+    for (int i = 0; i < 26; i++)
+    {
+        *(cp + i) = 'a' + i;
+    }
+    *(cp + 26) = 0;
+
+    for (int i = 0; i < 26; i++)
+        printf("%c", cp[i]);
+
+    free(cp);
+
+    return 0;
+}
